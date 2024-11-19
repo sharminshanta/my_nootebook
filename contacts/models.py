90bb3d06907718ab22a3b9a2a84ff8f721ba69ba
Sharmin Shanta <mimetyshanta@gmail.com>
2024-11-19 23:51:07 +0600

    Added new columns.

--- contacts/models.py
+++ contacts/models.py
@@ -1,5 +1,9 @@
+from django.utils import timezone
 from django.db import models
 
 class Contact(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  email = models.CharField(max_length=255)
+  phone = models.IntegerField()
+  timestamp = models.DateTimeField(default=timezone.now)
